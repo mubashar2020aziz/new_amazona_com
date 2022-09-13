5be5b6ab61d8f5dbaf988d23cb0abb02b49d9db9
mubshar aziz <mubasharaziz2020@gmail.com>
2022-09-13 12:57:46 +0400

    start frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>List of Producs</main>
+    </div>
+  );
+}
+
+export default App;
